e6699c7a8fa0565e2f05d58657e048bb3292e992
MeeraNair07 <meera07nair@gmail.com>
2022-04-11 07:34:40 +0300

    final

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ var scrollTop = document.getElementById("scroll-btn");
 window.onscroll = function(){
     scrollfunction()
 };
-function scrollfunction(){
 
+function scrollfunction(){
     if( document.body.scrollTop > 220 || document.documentElement.scrollTop > 220){
         scrollTop.style.display = "block";
     } else {
